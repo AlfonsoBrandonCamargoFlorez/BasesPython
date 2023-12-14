c9f60f064aa03edabad4abc4d6fb85aab8e328b6
AlfondoBrandonCamargoFlorez <b.c.florez@hotmail.com>
2023-12-14 17:09:10 -0500

    nuevos ejercicios

---
+++ Ejercicio5.py
@@ -0,0 +1,10 @@
+
+#se usa float para converitr la variable a un numero flotante
+a=float(input("Ingresa a"))
+b=float(input("Ingresa b")) 
+#** significa la elevacion d eun numero al cuadrado
+c = a**2 + b**2
+#se eleva  a 1/2 para reemplazar la raiz cuadrada
+print(c**(1/2))
+
+
---
+++ Ejercicio5_1.py
@@ -0,0 +1,10 @@
+#el math es parta importar la libreria de matematicas
+import math
+
+a=float(input("Ingresa a: "))
+b=float(input("Ingresa b: "))
+
+c = a**2 + b**2
+
+print(math.sqrt(c))
+
